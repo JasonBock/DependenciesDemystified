08391da29b7830ea35466f87aecb13626d3bbcf7
Jason Bock <jason.r.bock@outlook.com>
2021-07-22 07:05:32 -0500

    Updating examples

--- DependenciesDemystified.Client/Program.cs
+++ DependenciesDemystified.Client/Program.cs
@@ -9,141 +9,119 @@ using Microsoft.Extensions.DependencyInjection;
 using Spackle;
 using System;
 
-namespace DependenciesDemystified.Client
+//RunHardCodedChild();
+//RunDependentChild();
+RunWithServiceCollection();
+//RunWithAutofac();
+//RunWithCoreIntegration();
+
+static void RunHardCodedChild()
 {
-	class Program
+	Console.Out.WriteLine(nameof(RunHardCodedChild));
+
+	var child = new HardCodedChild();
+
+	for (var i = 0; i < 1000; i++)
 	{
-		//Program.RunHardCodedChild();
-		//Program.RunDependentChild();
-		//Program.RunWithSimpleContainer();
-		//Program.RunWithAutofac();
-		//Program.RunWithCoreIntegration();
-		static void Main() =>
-			Program.RunWithServiceCollection();
-
-		private static void RunHardCodedChild()
-		{
-			Console.Out.WriteLine(nameof(Program.RunHardCodedChild));
-
-			var child = new HardCodedChild();
-
-			for (var i = 0; i < 1000; i++)
-			{
-				child.DemandFunds();
-			}
-
-			Console.Out.WriteLine(child.Wallet);
-			Console.Out.WriteLine(child.Parent.Name);
-		}
-
-		private static void RunDependentChild()
-		{
-			Console.Out.WriteLine(nameof(Program.RunDependentChild));
-
-			var childUsesJason = new DependentChild(new JasonAsParent());
-
-			for (var i = 0; i < 1000; i++)
-			{
-				childUsesJason.DemandFunds();
-			}
-
-			Console.Out.WriteLine($"{nameof(childUsesJason)} - {childUsesJason.Wallet}");
-
-			var childUsesLiz = new DependentChild(new LizAsParent(new SecureRandom()));
-
-			for (var i = 0; i < 1000; i++)
-			{
-				childUsesLiz.DemandFunds();
-			}
-
-			Console.Out.WriteLine($"{nameof(childUsesLiz)} - {childUsesLiz.Wallet}");
-		}
-
-		private static void RunWithSimpleContainer()
-		{
-			Console.Out.WriteLine(nameof(Program.RunWithSimpleContainer));
-
-			var container = new SimpleContainer();
-			container.Register<IParent>(c => new JasonAsParent());
-			container.Register<IChild>(c => new DependentChild(c.Resolve<IParent>()));
-
-			var child = container.Resolve<IChild>();
-
-			for (var i = 0; i < 1000; i++)
-			{
-				child.DemandFunds();
-			}
-
-			Console.Out.WriteLine(child.Wallet);
-		}
-
-		private static void RunWithAutofac()
-		{
-			Console.Out.WriteLine(nameof(Program.RunWithAutofac));
-
-			var config = new ConfigurationBuilder();
-			config.AddJsonFile("autofac.json");
-
-			var builder = new ContainerBuilder();
-			builder.RegisterModule<CoreModule>();
-			builder.RegisterModule(new ConfigurationModule(config.Build()));
-
-			var container = builder.Build();
-
-			using var scope = container.BeginLifetimeScope();
-			var child = scope.Resolve<IChild>();
-
-			for (var i = 0; i < 100; i++)
-			{
-				child.DemandFunds();
-			}
-
-			Console.Out.WriteLine(child.Wallet);
-			Console.Out.WriteLine(child.Parent.Name);
-		}
-
-		private static void RunWithServiceCollection()
-		{
-			var services = new ServiceCollection();
-			services.AddScoped<IChild, DependentChild>();
-			services.AddScoped<IParent, JasonAsParent>();
-
-			var provider = services.BuildServiceProvider();
-
-			using var scope = provider.CreateScope();
-			var child = scope.ServiceProvider.GetService<IChild>();
-
-			for (var i = 0; i < 100; i++)
-			{
-				child.DemandFunds();
-			}
-
-			Console.Out.WriteLine(child.Wallet);
-			Console.Out.WriteLine(child.Parent.Name);
-		}
-
-		// https://docs.autofac.org/en/latest/integration/aspnetcore.html
-		private static void RunWithCoreIntegration()
-		{
-			var services = new ServiceCollection();
-
-			var builder = new ContainerBuilder();
-			builder.Populate(services);
-			builder.RegisterModule<CoreModule>();
-
-			var container = builder.Build();
-			var provider = new AutofacServiceProvider(container) as IServiceProvider;
-
-			using var scope = provider.CreateScope();
-			var child = scope.ServiceProvider.GetService<IChild>();
-
-			for (var i = 0; i < 100; i++)
-			{
-				child.DemandFunds();
-			}
-
-			Console.Out.WriteLine(child.Wallet);
-			Console.Out.WriteLine(child.Parent.Name);
-		}
+		child.DemandFunds();
 	}
+
+	Console.Out.WriteLine(child.Wallet);
+	Console.Out.WriteLine(child.Parent.Name);
+}
+
+static void RunDependentChild()
+{
+	Console.Out.WriteLine(nameof(RunDependentChild));
+
+	var childUsesJason = new DependentChild(new JasonAsParent());
+
+	for (var i = 0; i < 1000; i++)
+	{
+		childUsesJason.DemandFunds();
+	}
+
+	Console.Out.WriteLine($"{nameof(childUsesJason)} - {childUsesJason.Wallet}");
+
+	var childUsesLiz = new DependentChild(new LizAsParent(new SecureRandom()));
+
+	for (var i = 0; i < 1000; i++)
+	{
+		childUsesLiz.DemandFunds();
+	}
+
+	Console.Out.WriteLine($"{nameof(childUsesLiz)} - {childUsesLiz.Wallet}");
+}
+
+static void RunWithServiceCollection()
+{
+	Console.Out.WriteLine(nameof(RunWithServiceCollection));
+
+	var services = new ServiceCollection();
+	services.AddScoped<IChild, DependentChild>();
+	services.AddScoped<IParent, JasonAsParent>();
+
+	var provider = services.BuildServiceProvider();
+
+	using var scope = provider.CreateScope();
+	var child = scope.ServiceProvider.GetService<IChild>()!;
+
+	for (var i = 0; i < 100; i++)
+	{
+		child.DemandFunds();
+	}
+
+	Console.Out.WriteLine(child.Wallet);
+	Console.Out.WriteLine(child.Parent.Name);
+}
+
+static void RunWithAutofac()
+{
+	Console.Out.WriteLine(nameof(RunWithAutofac));
+
+	var config = new ConfigurationBuilder();
+	config.AddJsonFile("autofac.json");
+
+	var builder = new ContainerBuilder();
+	builder.RegisterModule<CoreModule>();
+	builder.RegisterModule(new ConfigurationModule(config.Build()));
+
+	var container = builder.Build();
+
+	using var scope = container.BeginLifetimeScope();
+	var child = scope.Resolve<IChild>();
+
+	for (var i = 0; i < 100; i++)
+	{
+		child.DemandFunds();
+	}
+
+	Console.Out.WriteLine(child.Wallet);
+	Console.Out.WriteLine(child.Parent.Name);
+}
+
+// https://docs.autofac.org/en/latest/integration/aspnetcore.html
+static void RunWithCoreIntegration()
+{
+	Console.Out.WriteLine(nameof(RunWithCoreIntegration));
+
+	var services = new ServiceCollection();
+
+	var builder = new ContainerBuilder();
+	builder.Populate(services);
+	builder.RegisterModule<CoreModule>();
+
+	var container = builder.Build();
+	var provider = new AutofacServiceProvider(container) as IServiceProvider;
+
+	using var scope = provider.CreateScope();
+	var child = scope.ServiceProvider.GetService<IChild>()!;
+
+	for (var i = 0; i < 100; i++)
+	{
+		child.DemandFunds();
+	}
+
+	Console.Out.WriteLine(child.Wallet);
+	Console.Out.WriteLine(child.Parent.Name);
 }
